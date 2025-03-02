79859ce648ddc6220e14826f9b405446f6956332
Terrasse <terrasse@qq.com>
2025-03-02 20:19:35 +0800

    support preventing black borders when deal_cover

--- deal_cover.py
+++ deal_cover.py
@@ -5,6 +5,8 @@ import os
 
 def deal_cover(cover_path):
     img = cv2.imread(cover_path)
+    crop_border = 10 # prevent black borders
+    img = img[crop_border:-crop_border, crop_border:-crop_border]
     bg = np.zeros((1700, 3000, 3), np.uint8)
     bg[:, :, :] = 255
     h, w, _ = img.shape
