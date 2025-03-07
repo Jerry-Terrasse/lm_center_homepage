127d399a0b78e11c5365135109b2aa845529b3d0
Terrasse <terrasse@qq.com>
2025-03-07 21:23:55 +0800

    add non-ascii check

--- release.sh
+++ release.sh
@@ -4,6 +4,13 @@ set -e
 
 rm -rf ./public
 hugo build -b https://cs.nju.edu.cn/lm/
+
+# Check if there are non-ASCII characters in the output
+if find ./public -print | grep -P "[^\x00-\x7F]" > /dev/null; then
+    echo -e "\033[31mError: Non-ASCII characters found in the output\033[0m"
+    exit 1
+fi
+
 python update_index.py
 cp ./public/index.html ./public/index.htm
 
